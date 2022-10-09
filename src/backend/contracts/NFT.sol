60ce1f233b528bfab6117242b0a17a39d5cdd14a
Stuti Patel <82572025+stutidpatel@users.noreply.github.com>
2022-10-09 14:34:49 +0530

    commit

---
+++ src/backend/contracts/NFT.sol
@@ -0,0 +1,23 @@
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.4;
+    import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+    contract NFT is ERC721URIStorage{
+        address payable owner;
+        uint public curTokenId; //token count and token ID
+        constructor() ERC721("NFT Marketplace","NFT") {
+        
+        }
+        // can only be called from outside the contract
+        function mint(string memory _tokenURI) external returns (uint){
+            curTokenId++;
+
+            // _safeMint(to, tokenId) -> safely mint a new token
+            _safeMint(msg.sender, curTokenId);
+
+            // _setTokenURI(uint256 tokenId, string _tokenURI) -> called while creating token (create.js) 
+            // set with pinata url
+            _setTokenURI(curTokenId, _tokenURI);
+            return curTokenId;
+        }
+    }
